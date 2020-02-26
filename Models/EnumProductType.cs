a75d272c3a1bd2adca37d89af23f958142e7e436
Qassamala <abdigobena@hotmail.com>
2020-02-26 13:32:26 +0100

    Created Enum EnumProductType, and class Product with properties

---
+++ Models/EnumProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagementApi.Models
+{
+    public enum EnumProductType
+    {
+        Pen,
+        Notebook,
+        Paper,
+        Eraser
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagementApi.Models
+{
+    public class Product
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public EnumProductType ProductType { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
