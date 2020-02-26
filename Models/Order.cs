bd8e92b12e9b2c24b8c7d3276ad429b4d3b575a1
Qassamala <abdigobena@hotmail.com>
2020-02-26 13:56:50 +0100

    Added class Order, with properties

---
+++ Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagementApi.Models
+{
+    public class Order
+    {
+        [Key]
+        public long Id { get; set; }
+        
+        [Required]
+        [ForeignKey("Customer")]
+        public long CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public decimal TotalSum { get; set; }
+
+        public decimal TotalDiscount { get; set; }
+
+        public virtual ICollection<OrderRow> Rows { get; set; }
+    }
+}
