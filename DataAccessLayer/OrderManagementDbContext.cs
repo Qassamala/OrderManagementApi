b23340e7cb23ff47e6754102568fd0ebed5c1f6a
Qassamala <abdigobena@hotmail.com>
2020-02-26 14:09:57 +0100

    Added DbContext

---
+++ DataAccessLayer/OrderManagementDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagementApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagementApi.DataAccessLayer
+{
+    public class OrderManagementDbContext : DbContext
+    {
+        public OrderManagementDbContext(DbContextOptions<OrderManagementDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderRow> OrderRows { get; set; }
+    }
+}
