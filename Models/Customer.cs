c139de93a777de96cd9458b8d1cc2bb34786f482
Qassamala <abdigobena@hotmail.com>
2020-02-26 13:37:34 +0100

    Added class Customer with properties

---
+++ Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagementApi.Models
+{
+    public class Customer
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public EnumCustomerType CustomerType { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
