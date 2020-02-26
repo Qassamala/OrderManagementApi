57e1472b7d60ffc18c7c800d6566e216db44fea4
Qassamala <abdigobena@hotmail.com>
2020-02-26 13:56:21 +0100

    Created class OrderRow to enable support for several orderrows for one order

---
+++ Models/OrderRow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderManagementApi.Models
+{
+    public class OrderRow
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public int? Quantity { get; set; }
+        [Required]
+        public decimal? SingleProductPrice { get; set; }
+
+        public decimal TotalSum { get; set; }
+        public decimal TotalDiscount { get; set; }
+
+        [Required]
+        [ForeignKey("Product")]
+        public long? ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        [Required]
+        [ForeignKey("Order")]
+        public long? OrderId { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
