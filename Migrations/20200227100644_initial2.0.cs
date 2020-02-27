c633ca2938fcca515407a170450713d1b6b85705
Qassamala <abdigobena@hotmail.com>
2020-02-27 13:48:09 +0100

    Created new migration to update database schema

---
+++ Migrations/20200227100644_initial2.0.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrderManagementApi.Migrations
+{
+    public partial class initial20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductType",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductType",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
